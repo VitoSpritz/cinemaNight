3e84d2f599bcd61ed4319d7ec620f396470a5f2d
giorgio <giorgio.zappoli@studio.unibo.it>
2025-09-30 23:07:13 +0200

    feat(UI): :sparkles: finish login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 import 'router/router.dart';
+import 'sign_in.dart';
 
 class LoginScreen extends StatelessWidget {
   final String path = '/login';
@@ -12,67 +11,104 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: const Text(
-              "Login",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 90),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: const Text(
+                "Login",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 80),
+              ),
             ),
-          ),
-          const SizedBox(height: 16),
+            const SizedBox(height: 50),
 
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                label: Text(
-                  'Email',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  label: Text(
+                    'Email',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
-          ),
 
-          const SizedBox(height: 69),
+            const SizedBox(height: 69),
 
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                ),
-                label: Text(
-                  'Password',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  label: Text(
+                    'Password',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 69),
+            const SizedBox(height: 69),
 
-          FractionallySizedBox(
-            widthFactor: 0.85,
-            child: FilledButton(
-              onPressed: () => router.push("/login"),
-              style: FilledButton.styleFrom(
-                backgroundColor: Colors.amberAccent,
-                foregroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(10),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: FilledButton(
+                onPressed: () => router.push("/login"),
+                style: FilledButton.styleFrom(
+                  backgroundColor: Colors.amberAccent,
+                  foregroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10),
+                  ),
                 ),
+                child: const Text("Login"),
               ),
-              child: const Text("Login"),
             ),
-          ),
-        ],
+
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.80,
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    "Not registered? Press ",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 24,
+                    ),
+                  ),
+
+                  const SizedBox(height: 200),
+
+                  TextButton(
+                    onPressed: () => router.push(SignInScreen.path),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                      minimumSize: const Size(0, 0),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    child: const Text(
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 24,
+                      ),
+                      "here",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'home.dart';
 import 'login.dart';
 import 'router/router.dart';
 
