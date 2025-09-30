7ee30fa9610f6d638cdf67303c9c8b10635e2312
Vito Paolo <potenzavitopaolo@gmail.com>
2025-09-30 23:01:01 +0200

    feat(Back end): :construction: add firebase authenticator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'auth/authenticator.dart';
+import 'firebase_options.dart';
 import 'screens/router/router.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    ChangeNotifierProvider(
+      create: (BuildContext context) => Authenticator(),
+      builder: ((BuildContext context, Widget? child) => const MainApp()),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
