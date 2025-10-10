1513f03f5d9742fbdeb1a32c27d548c0d6355a6f
giorgio <giorgio.zappoli@studio.unibo.it>
2025-10-10 23:37:37 +0200

    feat(uI): :sparkles: add icon to topbar and pubspec

--- lib/widget/custom_app_bar.dart
+++ lib/widget/custom_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../consts/custom_colors.dart';
 import 'custom_icon_button.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -19,11 +20,14 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       toolbarHeight: 45,
-      backgroundColor: const Color(0xFFF7B921),
+      backgroundColor: CustomColors.mainYellow,
       titleSpacing: 0,
       title: Row(
         children: <Widget>[
-          Image.asset('assets/images/logo.png', width: 60),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, top: 12, bottom: 12),
+            child: Image.asset('assets/images/namelesslogo.png', width: 40),
+          ),
           Expanded(
             child: Center(
               child: Text(
@@ -40,8 +44,8 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
             CustomIconButton(
               icon: Icons.search,
               onTap: () {},
-              size: 45,
-              color: const Color(0xFF002F4E),
+              iconSize: 45,
+              color: CustomColors.text,
             )
           else
             const SizedBox(width: 60),
