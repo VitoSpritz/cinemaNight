480f5467aa24f00bca4e02dffeeebb853ace9ef2
VitoSpritz <potenzavitopaolo@gmail.com>
2025-10-14 21:16:28 +0200

    feat(back-end): :sparkles: add env class and deps

---
+++ lib/env/env.dart
@@ -0,0 +1,16 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: "TOKEN", obfuscate: true)
+  static final String token = _Env.token;
+
+  @EnviedField(varName: "API_KEY", obfuscate: true)
+  static final String apiKey = _Env.apiKey;
+
+  @EnviedField(varName: "BASE_URL", obfuscate: true)
+  static final String baseUrl = _Env.baseUrl;
+}
