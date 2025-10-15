1e467ce86de790b7abce283930e9f0f7d458446f
VitoSpritz <potenzavitopaolo@gmail.com>
2025-10-15 23:38:52 +0200

    feat(back-end): :sparkles: add methods to fetch poster to the api

--- lib/interface/tmdb_storage.dart
+++ lib/interface/tmdb_storage.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
+
+import '../model/media_with_poster.dart';
 import '../model/movie.dart';
-import '../model/tvShow.dart';
+import '../model/multi.dart';
+import '../model/tv_show.dart';
 
 abstract class TmdbStorage {
   Future<List<Movie>> getMovieByName({
@@ -7,4 +11,13 @@ abstract class TmdbStorage {
     required String language,
   });
   Future<List<TvShow>> getTvShowByName({required String name});
+  Future<List<Multi>> getMultiMediaByName({
+    required String name,
+    required String language,
+  });
+  Future<Uint8List> getMediaPoster({required String posterPath});
+  Future<List<MediaWithPoster>> getMultiMediaWithPosters({
+    required String name,
+    required String language,
+  });
 }
--- lib/providers/tmdb_api.dart
+++ lib/providers/tmdb_api.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
 
 import '../env/env.dart';
 import '../interface/tmdb_storage.dart';
 import '../model/http_status.dart';
+import '../model/media_with_poster.dart';
 import '../model/movie.dart';
-import '../model/tvShow.dart';
+import '../model/multi.dart';
+import '../model/tv_show.dart';
 
 class TmdbApi implements TmdbStorage {
   @override
@@ -27,14 +30,17 @@ class TmdbApi implements TmdbStorage {
 
     final http.Response response = await http.get(url);
 
-    if (response.statusCode == HttpStatus.ok.code) {
-      final Map<String, dynamic> data = jsonDecode(response.body);
-      final List<dynamic> results = data['results'] ?? <dynamic>[];
-
-      return Movie.parseList(results);
-    } else {
+    if (response.statusCode != HttpStatus.ok.code) {
       throw Exception('Failed to load movies ${response.statusCode}');
     }
+
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    if (!data.containsKey('results')) {
+      throw Exception('No results fetched');
+    }
+
+    final List<dynamic> results = data['results'];
+    return results.map((json) => Movie.fromJson(json)).toList();
   }
 
   @override
@@ -51,13 +57,94 @@ class TmdbApi implements TmdbStorage {
 
     final http.Response response = await http.get(url);
 
-    if (response.statusCode == HttpStatus.ok.code) {
-      final Map<String, dynamic> data = jsonDecode(response.body);
-      final List<dynamic> results = data['results'] ?? <dynamic>[];
-
-      return TvShow.parseList(results);
-    } else {
+    if (response.statusCode != HttpStatus.ok.code) {
       throw Exception('Failed to load tv shows ${response.statusCode}');
     }
+
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    if (!data.containsKey('results')) {
+      throw Exception('No results fetched');
+    }
+
+    final List<dynamic> results = data['results'];
+    return results.map((json) => TvShow.fromJson(json)).toList();
+  }
+
+  @override
+  Future<List<Multi>> getMultiMediaByName({
+    required String name,
+    required String language,
+  }) async {
+    final Map<String, String> queryParams = <String, String>{
+      'query': name,
+      'include_adult': 'true',
+      'language': language,
+      'api_key': Env.apiKey,
+    };
+
+    final Uri url = Uri.parse(
+      '${Env.baseUrl}/3/search/multi',
+    ).replace(queryParameters: queryParams);
+
+    final http.Response response = await http.get(url);
+
+    if (response.statusCode != HttpStatus.ok.code) {
+      throw Exception('Failed to load tv show ${response.statusCode}');
+    }
+
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    if (!data.containsKey('results')) {
+      throw Exception('No results fetched');
+    }
+    final List<dynamic> results = data['results'];
+
+    return results.map((json) => Multi.fromJson(json)).toList();
+  }
+
+  @override
+  Future<Uint8List> getMediaPoster({required String posterPath}) async {
+    final Uri url = Uri.parse('https://image.tmdb.org/t/p/w500$posterPath');
+
+    final http.Response response = await http.get(
+      url,
+      headers: <String, String>{'Authorization': 'Bearer ${Env.token}'},
+    );
+
+    if (response.statusCode != HttpStatus.ok.code) {
+      throw Exception('Failed to load poster ${response.statusCode}');
+    }
+
+    return response.bodyBytes;
+  }
+
+  @override
+  Future<List<MediaWithPoster>> getMultiMediaWithPosters({
+    required String name,
+    required String language,
+  }) async {
+    final List<Multi> mediaList = await getMultiMediaByName(
+      name: name,
+      language: language,
+    );
+
+    final List<MediaWithPoster> result = <MediaWithPoster>[];
+
+    for (final Multi media in mediaList) {
+      Uint8List? posterBytes;
+
+      if (media.posterPath != null && media.posterPath!.isNotEmpty) {
+        try {
+          posterBytes = await getMediaPoster(posterPath: media.posterPath!);
+        } catch (e) {
+          // ignore: avoid_print
+          print('Failed to load poster for ${media.title}: $e');
+          posterBytes = null;
+        }
+      }
+
+      result.add(MediaWithPoster(media: media, posterBytes: posterBytes));
+    }
+
+    return result;
   }
 }
